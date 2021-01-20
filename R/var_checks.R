21a0f474fbdd05572880c3c4c1a784d4b65a1f6d
Ben Straub <ben.x.straub@gsk.com>
2021-01-20 11:27:02 -0500

    Messing with roxygen headers and name of file

---
+++ R/var_checks.R
@@ -0,0 +1,29 @@
+#' Variable Checks 1
+#'
+#' @return
+#' @export
+#'
+#' @examples
+function(){
+  
+}
+
+#' Variable Checks 1
+#'
+#' @return
+#' @export
+#'
+#' @examples
+function(){
+  
+}
+
+#' Variable Checks 1
+#'
+#' @return
+#' @export
+#'
+#' @examples
+function(){
+  
+}
