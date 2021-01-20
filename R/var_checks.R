c8fddd5d774b718f509f8e0808670e58b557e98d
Ben Straub <ben.x.straub@gsk.com>
2021-01-20 15:13:42 -0500

    uploaded some sas datasets.  Started function for checking case of variable name.

--- R/var_checks.R
+++ R/var_checks.R
@@ -1,4 +1,21 @@
-#' Variable Checks 1
+#' UPPER CASE check
+#'
+#' Check to see if data set has all upper case for variables
+#' @param .data Data frame containing data to calculate summary statistics for
+#' @importFrom purrr map map2 reduce
+#' @importFrom rlang quos quo_name quo_is_null enquo
+#' @return Tibble of Variables flagged with lower case
+#' @export
+#'
+#' @examples
+check_var_case <- function(.data){
+    bind_cols(colnames(.data), 
+            str_detect(colnames(.data), "[A-Z0-9]+(?:[A-Z0-9]+)+")) %>% 
+    rename("Variable"=...1, "Flag"=...2) %>% 
+    filter(Flag == "FALSE")
+}
+
+#' Variable Checks 2
 #'
 #' @return
 #' @export
@@ -8,17 +25,7 @@ function(){
   
 }
 
-#' Variable Checks 1
-#'
-#' @return
-#' @export
-#'
-#' @examples
-function(){
-  
-}
-
-#' Variable Checks 1
+#' Variable Checks 3
 #'
 #' @return
 #' @export
---
+++ R/var_checks_across.R
@@ -0,0 +1,19 @@
+#' All Variables for all datasets are in UPPER CASE
+#'
+#' @return
+#' @export
+#'
+#' @examples
+function(){
+  
+}
+
+#' Character Variables do not exceed 200 Characters
+#'
+#' @return
+#' @export
+#'
+#' @examples
+function(){
+  
+}
