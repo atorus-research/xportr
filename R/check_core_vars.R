c8d614ca563ce311a3050068c008f6e492b95995
Ross <Rostyslav.Didenko@atorusresearch.com>
2021-01-27 19:04:38 +0200

    1. Added checks for permissible, expected and conditionally required variables. 2. Put all checks under single function 'check_core'. 3. Still need to add docstrings.

--- R/check_core_vars.R
+++ R/check_core_vars.R
@@ -40,9 +40,6 @@ library('dplyr')
 library('stringr')
 library('SASxport')
 
-# TODO: Use 'assertthat' instead of 'testthat'.
-# TODO: Refer to Eli's code on how to throw warnings.
-
 # Before proceed to checking variables we need to load specification and make sure
 # there is a column, named "Core" as well as "Variable" column.
 #' Load spec from a file.
@@ -56,16 +53,14 @@ load_spec <- function(spec, sheet = "Variables"){
   s <- read_excel(spec, sheet=sheet)
 
   # Check that spec contains "Variables" column.
-  expect_true(
-    any(stringr::str_detect(string = names(s), stringr::regex("variable", TRUE))),
-    label = "Column 'Variable' was not found in selected spec."
-  )
+  if ( !any(stringr::str_detect(string = names(s), stringr::regex("variable", TRUE))) ){
+    stop("Column 'Variable' was not found in selected spec.")
+  }
 
   # Check that spec contains "Core" column.
-  expect_true(
-    any(stringr::str_detect(string = names(s), stringr::fixed("core", TRUE))),
-    label = "Column 'Core' was not found in selected spec."
-  )
+  if ( !any(stringr::str_detect(string = names(s), stringr::fixed("core", TRUE))) ){
+    stop("Column 'Core' was not found in selected spec.")
+  }
 
   return (s)
 }
@@ -78,18 +73,15 @@ load_spec <- function(spec, sheet = "Variables"){
 #' @example miss_count('adae.xpt', 'ASTDT')
 miss_count <- function(dataset., variable.){
   data <- read.xport(dataset.)
-  expect_true(
-    any(stringr::str_detect(string = names(data), stringr::regex(variable., TRUE))),
-    label = paste0("Column ", variable., " was not found in dataset.")
-  )
+
+  if ( !any(stringr::str_detect(string = names(data), stringr::regex(variable., TRUE))) ){
+    stop(paste0("Column ", variable., " was not found in dataset."))
+  }
+
   return(
     sum(is.na(data[variable.]))
   )
 }
-
-
-# Checkpoint #1 - Load spec.
-# d <- load_spec(spec = "ADaM_spec.xlsx", sheet = "Variables")
 
 
 #' Extract Variables and respecive categories.
@@ -107,19 +99,9 @@ get_core_vars_cat <- function(spec){
   )
 }
 
-# Checkpoint #2 - Extract vars and their categories.
-# vars <- get_core_vars_cat(d)
 
-
-#' Check if required variable from the spec is present in the data. Also check if non of the required variables contain
-#' missing values.
-#' @details
-#' @param
-#' @return
-#' @examples
-#'
-check_req_vars <- function(spec., dataset., ds_name. = ""){
-    # Assign dataset name to 'ds' or try to take from 'dataset.' param.
+check_core <- function(spec., dataset., ds_name. = "", var_categ = c("req", "exp", "perm", "cond")){
+  # Assign dataset name to 'ds' or try to take from 'dataset.' param.
   if (missing(ds_name.)){
     ds <- tools::file_path_sans_ext(dataset.)
   }  else{
@@ -132,52 +114,94 @@ check_req_vars <- function(spec., dataset., ds_name. = ""){
   # Get list of Variables and their respective Core category.
   core_vars_w_cats <- get_core_vars_cat(spec.)
 
-  # Obtain list of Required variables, which should be present in the dataset.
-  req_vars_list <- core_vars_w_cats %>%
-    select_if(function (col) stringr::str_detect(string = col, stringr::regex('req', TRUE))) %>%
-    names()
-
   # Obtain list of Variables actually present in the dataset.
   ds_vars_list <- names(read.xport(dataset.))
 
-  vars_not_in_dataset <- setdiff(req_vars_list, ds_vars_list)
+  # Calculate number of observations in dataset.
+  obs <- nrow(read.xport(dataset.))
 
-  # Check for any 'req' variable not present in dataset.
-  if (length(vars_not_in_dataset) > 0) {
-    stop(paste0("Required variable(-s)", vars_not_in_dataset, " are not present in ", dataset., "."))
-  }
+  # Obtain list of certain type variables ('req', 'exp', 'perm' - specified in function call in 'var_categ'),
+  # which should be present in the dataset.
+  for (type in tolower(var_categ)){
+    vars_list <- core_vars_w_cats %>%
+    select_if(function (col) stringr::str_detect(string = col, stringr::regex(type, TRUE))) %>%
+      names()
 
-  # Check for any 'req' variable has missing value.
-  miss_list <- ""
-  for (var in ds_vars_list){
-    if (miss_count(dataset., var)){
-      miss_list <- append(miss_list, var)
+    # Generate list of variables present in metadata and not in dataset.
+    vars_not_in_dataset <- setdiff(vars_list, ds_vars_list)
+    core_vars_in_data <- intersect(vars_list, ds_vars_list)
+
+    # Generate list of variables with missing values.
+    miss_list_all <- ""
+    miss_list_any <- ""
+    for (var in core_vars_in_data){
+      if (miss_count(dataset., var)){
+        miss_list_any <- append(miss_list_any, var)
+      }
+      if (miss_count(dataset., var) == obs){
+        miss_list_all <- append(miss_list_all, var)
+      }
     }
+    miss_list_any <- miss_list_any[miss_list_any != ""]
+    miss_list_all <- miss_list_all[miss_list_all != ""]
+
+    # <--- Required vars ---> #
+    if (type == 'req'){
+      # If ANY 'req' variable is missing or has NA - stop.
+      if (length(vars_not_in_dataset) > 0) {
+        stop(paste0("Required variable(-s) ", vars_not_in_dataset, " are not present in ", dataset., "."))
+      }
+      if (length(miss_list_any) > 0){
+        stop(paste0("Required variable(-s) ", paste0(miss_list_any, collapse = ' '), " in ", dataset., " contains missing values!
+        Required variables must always be included in the dataset and cannot be null for any record. Please refer to
+        https://www.cdisc.org/ for more details."))
+      }
+    }
+
+    # <--- Expected vars ---> #
+    if (type == 'exp'){
+      # If ANY 'exp' variable is missing - stop.
+      if (length(vars_not_in_dataset) > 0) {
+        stop(paste0("Expected variable(-s) ", vars_not_in_dataset, " are not present in ", dataset., ". When the study
+        does not include the data item for an expected variable, however, a null column must still be included in the
+        dataset, and a comment must be included in the Define-XML document to state that the study does not include
+        the data item. Please refer to https://www.cdisc.org/ for more details."))
+      }
+      # If ALL values of the expected variable are NA - put a warning.
+      if (length(miss_list_all) > 0){
+        warning(paste0("Expected variable(-s) ", paste0(miss_list_all, collapse = ' '), " in ", dataset., " has only NA values.
+        Make sure to include comment in the Define-XML document to state that the study does not include the
+        data item for the particular expected variable. Please refer to https://www.cdisc.org/ for more details."))
+      }
+    }
+
+    # <--- Permissible vars ---> #
+    if (type == 'perm'){
+      # If ALL values of the permissible variable are NA - put a warning.
+      if (length(miss_list_all) > 0){
+        warning(paste0("Permissible variable(-s) ", paste0(miss_list_all, collapse = ' '), " in ", dataset., " has only NA values.
+        Check if a study includes a data item that is represented in the particular permissible variable. If yes -
+        make sure to include comment in the Define-XML document to indicate no data were available for that variable.
+        Otherwise - remove the variable from dataset. Please refer to https://www.cdisc.org/ for more details."))
+      }
+    }
+
+        # <--- Permissible vars ---> #
+    if (type == 'cond'){
+      # If ALL values of the conditionally required variable are NA - put a warning.
+      if (length(miss_list_all) > 0){
+        warning(paste0("Conditionally required variable(-s) ", paste0(miss_list_all, collapse = ' '), " in ", dataset., " has
+        only NA values.
+        Check if a study includes a data item that is represented in the particular variable. If yes -
+        make sure to include comment in the Define-XML document to indicate no data were available for that variable.
+        Otherwise - remove the variable from dataset. Please refer to https://www.cdisc.org/ for more details."))
+      }
+    }
+
   }
 
-  if (length(miss_list) > 0){
-    stop(paste0("Required variable", paste0(miss_list, collapse = ' '), " in ", dataset., " contains missing values!
-    It shouldn't per CDISC. Please refer to https://www.cdisc.org/ for more details."))
-  }
-
-  return (miss_list)
 }
 
-# Checkpoint #3 - Alarm when 'req' var is not present.
-# check_req_vars(d, "adae.xpt")
-
-#' @example
-#'d <- load_spec("ADaM_spec.xlsx",
-#'               "Variables") %>%
-#'  check_req_vars_present("adae.xpt")
-#'
-#'d <- load_spec("ADaM_spec.xlsx",
-#'               "Variables") %>%
-#'  check_req_vars_present("adae_rd.xpt", "ADAE")
-
-# Checkpoint #4 - Final Function 1 call.
+# Checkpoint #5 - Final.
 d <- load_spec("ADaM_spec.xlsx") %>%
-  check_req_vars("adae.xpt")
-
-
-#TODO: function to check 'exp' vars for missings.
+  check_core("adae.xpt")
