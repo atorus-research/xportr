5e8fea873b9ed4f7f3c2fe431d8c04a2a1e9d667
Vignesh T <vignesh.x.thanikachalam@gsk.com>
2021-02-08 14:59:14 +0530

    Add new file metadata.R test-metadata.R

---
+++ tests/testthat/test-metadata.R
@@ -0,0 +1,84 @@
+library(tibble)
+
+test_that("Variable label", {
+  df <- tibble(x = "a", y = "b")
+  varmeta <- tibble(dataset  = rep("df", 2), 
+                    variable = c("x", "y"), 
+                    label    = c("foo", "bar"))
+  
+  extract_varlabel <- function(.x) {
+    vapply(.x, function(.x) attr(.x, "label"), character(1), USE.NAMES = FALSE) 
+  }
+  
+  df <- define_varlabel(df, varmeta)
+  
+  expect_equal(extract_varlabel(df), c("foo", "bar"))
+  expect_output(str(df), "1 x 2")
+})
+
+test_that("Dataset label", {
+  df <- tibble(x = "a", y = "b")
+  dfmeta <- tibble(name  = "df", 
+                   label = "Label") 
+  
+  df <- define_dflabel(df, dfmeta)    
+  expect_equal(attr(df, "label"), "Label") 
+  expect_output(str(df), "1 x 2")
+})
+
+test_that("Expect error if any variable doesn't exist in var. metadata", {
+  df <- tibble(x = "a", y = "b")
+  varmeta <- tibble(dataset  = "df", 
+                    variable = "x",
+                    label    = "foo")
+  
+  expect_error(define_varlabel(df, varmeta), "present in input data but doesn't exist")
+})
+
+test_that("Expect error if any label exceeds 40 character", {
+  df <- tibble(x = "a", y = "b")
+  varmeta <- tibble(dataset  = rep("df", 2), 
+                    variable = c("x", "y"), 
+                    label    = c("foo", "Lorem ipsum dolor sit amet, consectetur adipiscing elit"))
+  dfmeta <- tibble(name  = "df", 
+                   label = "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+  
+  expect_error(define_varlabel(df, varmeta), "label exceeds 40 character")
+  expect_error(define_dflabel(df, dfmeta), "label exceeds 40 character")
+})
+
+test_that("SAS format", {
+  df <- tibble(x = 1, y = 2)
+  varmeta <- tibble(dataset  = rep("df", 2), 
+                    variable = c("x", "y"), 
+                    sas_format = c("date9.", "datetime20."))
+  
+  extract_format <- function(.x) {
+    vapply(.x, function(.x) attr(.x, "SASformat"), character(1), USE.NAMES = FALSE) 
+  }
+  
+  out <- define_format(df, varmeta)
+  
+  expect_equal(extract_format(out), c("DATE9.", "DATETIME20."))
+  expect_output(str(out), "1 x 2")
+})
+
+# test_that("SAS length", {
+#   df <- tibble(x = "a", y = "b")
+#   varmeta <- tibble(dataset  = rep("df", 2), 
+#                     variable = c("x", "y"), 
+#                     type     = c("text", "text"),
+#                     length   = c(1, 1))
+#   
+#   extract_length <- function(.x) {
+#     vapply(.x, function(.x) attr(.x, "SASlength"), character(1), USE.NAMES = FALSE) 
+#   }
+#   
+#   out <- define_length(df, varmeta)
+#   
+#   expect_equal(extract_length(out), c(1, 1))
+#   expect_output(str(out), "1 x 2")
+#   
+#   df <- cbind(df, z = 3)
+#   expect_error(define_length(df, varmeta), "doesn't exist")
+# })
