4796a74ddddbf154f5a9922c01ebee72f04e2369
Ben Straub <ben.x.straub@gsk.com>
2021-03-10 16:55:21 -0500

    Cleaned up test script.

--- tests/testthat/test-var-order.R
+++ tests/testthat/test-var-order.R
@@ -4,12 +4,11 @@ library(testthat)
 
 #context("xportr_seq correctly order dataset according to spec")
 
-
 test_that("Variable are ordered correctly", {
   
   ADAE <- read_sas("~/xptr/inst/extdata/adae.sas7bdat")
   
-  ADAE_xportr <- xportr_seq("ADAE", ADAE, tab_model = "ADAM", vendor = "GSK", verbose = FALSE)
+  ADAE_xportr <- xportr_ord("ADAE", ADAE, tab_model = "ADAM", vendor = "GSK", verbose = FALSE)
   
   vars_in_spec_ds <- c("STUDYID","SITEID", "USUBJID","SUBJID", "TRT01A", "TRT01AN",
                        "AETERM", "AEDECOD","AESOC","AGE","SEX","RACE","SAFFL","ASTDT",
@@ -29,23 +28,3 @@ test_that("Variable are ordered correctly", {
   expect_equal(ADAE_xportr, ADAE_manual)
 })
 
-# ADLB <- read_sas("~/xptr/inst/extdata/adlb.sas7bdat")
-# ADSL <- read_sas("~/xptr/inst/extdata/adsl.sas7bdat")
-# 
-# 
-# ADAE2 <- ADAE %>% 
-#   select(1:24) %>% 
-#   xportr_seq(tab_model = "ADAM", vendor = "GSK", verbose = FALSE)
-# 
-# 
-#ADAE <- xportr_seq(ADAE, tab_model = "ADAM", vendor = "GSK", verbose = FALSE)
-# 
-# ADLB <- xportr_seq(ADLB, tab_model = "ADAM", verbose = TRUE)
-# 
-# ADSL <- xportr_seq(ADSL, tab_model = "ADAM", verbose = TRUE)
-# 
-# 
-# ADAE %>% xportr_seq(tab_model = "ADAM", vendor = "GSK", verbose = FALSE)
-# 
-# ADLB <- xportr_seq("ADLB", ADLB, tab_model = "ADAM", verbose = FALSE)
-# ADSL <- xportr_seq("ADSL", ADSL, tab_model = "ADAM", verbose = TRUE)
