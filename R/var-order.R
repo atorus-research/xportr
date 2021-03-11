eac10091e45e090e44282c4fa77ba7c3bad112be
Ben Straub <ben.x.straub@gsk.com>
2021-03-10 22:06:50 -0500

    Working on applying ordering function to entire directory.

--- R/var-order.R
+++ R/var-order.R
@@ -107,7 +107,7 @@ xportr_ord <- function(df1, df2, tab_model = tab_model, vendor = vendor, verbose
   cli_div(theme = list(span.emph = list(color = "orange")))
   cli_alert_success("I have retrieved the {.emph {vendor}} {.emph {tab_model}} Spec for you.")
   
-  cli_h1("Starting to Order Variables according to Spec")
+  cli_h2("Starting to Order Variables according to {.emph {vendor}} {.emph {tab_model}} {df1} Spec")
   cli_text("")
   
   # Grabs vars from Spec and inputted dataset
@@ -144,9 +144,15 @@ xportr_ord <- function(df1, df2, tab_model = tab_model, vendor = vendor, verbose
   return(df_re_ord)
 }
 
-#' @title Apply Ordering to entire directory of datasets
+
+#################
+# This is not working - happy to hear ideas!
+#################
+
+#' @title Apply Ordering of Spec to entire directory of datasets
 #'
 #' @param path path to folder with datasets
+#' @param pattern Type of file for function to identify and reorder - sas7bdat, csv, xpt, Rdata, others?
 #' @param tab_model 
 #' @param vendor 
 #' @param verbose
@@ -155,32 +161,43 @@ xportr_ord <- function(df1, df2, tab_model = tab_model, vendor = vendor, verbose
 #'
 #' @return
 #' @export
-xportr_ord_dir <- function(path, tab_model, vendor, verbose){
+xportr_ord_dir <- function(path, pattern, tab_model = tab_model, vendor = vendor, verbose = verbose){
   
   # Supply path to directort
-  cdisc_data <- list.files(path = path, pattern = ".sas7bdat")
+  cdisc_data <- list.files(path = path, pattern = pattern)
   
-  cdisc_name_strip <- str_to_upper(str_remove(cdisc_data, ".sas7bdat"))
+  cdisc_name_strip <- str_to_upper(str_remove(cdisc_data, pattern))
+  
+  cdisc_name_lst <- as.list(cdisc_name_strip)
    
   cdisc_data_2 <- paste0("~/xptr/inst/extdata/", cdisc_data)
   
   cdisc_data_3 <- lapply(cdisc_data_2, read_sas)
   
-  for (i in 1:length(cdisc_data_2)) {
-    assign(paste0(cdisc_name_strip[i]), cdisc_data_3[[i]])
-  }
+  names(cdisc_data_3) <- cdisc_name_strip
+  
+  # for (i in 1:length(cdisc_data_2)) {
+  #   assign(paste0(cdisc_name_strip[i]), cdisc_data_3[[i]])
+  # }
   
   for (i in cdisc_name_strip){
-      
-        xportr_ord(df1 = as.character(cdisc_name_strip[i]), 
-                   df2 = cdisc_data_3[[i]],
-                   tab_model = tab_model, vendor = vendor, verbose = FALSE)
+    #for (i in 1:length(cdisc_name_strip)){
+    
+    print(cdisc_name_strip[i])
+    print(as.data.frame(cdisc_data_3[i]))
+    
+        # xportr_ord(df1 = cdisc_name_lst[i], 
+        #            df2 = as.data.frame(cdisc_data_3[i]),
+        #            tab_model = tab_model, vendor = vendor, verbose = verbose)
+    #}
   }
 }
    
-xportr_ord_dir(path = "~/xptr/inst/extdata/", tab_model = "ADAM", vendor = "GSK", verbose = FALSE)
-
-
+xportr_ord_dir(path = "~/xptr/inst/extdata/",
+               pattern = ".sas7bdat",
+               tab_model = "ADAM",
+               vendor = "GSK",
+               verbose = T)
 
 
 
