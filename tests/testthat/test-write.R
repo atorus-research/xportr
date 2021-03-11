3e1e02079a9fe91d831ef1423f3f6d91e2e09ca8
Vignesh T <vignesh.x.thanikachalam@gsk.com>
2021-03-11 18:32:43 +0530

    Renamed file test-xport.R to test-write.R

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -9,25 +9,25 @@ test_that("SAS Transport file", {
   SASxport::label(df$y) <- "bar"
   SASxport::label(df$z) <- "baz"
 
-  write_xport(df, path = tmp)
+  xportr_write(df, path = tmp)
   expect_output(str(SASxport::read.xport(tmp)), "$ X: labelled, format", fixed =TRUE)
   expect_output(str(SASxport::read.xport(tmp)), "$ Y: 'labelled' chr", fixed =TRUE)
   expect_output(str(SASxport::read.xport(tmp)), "$ Z: 'labelled' int", fixed =TRUE)
 
-  write_xport(df, path = tmp, label = "Lorem ipsum dolor sit amet")
+  xportr_write(df, path = tmp, label = "Lorem ipsum dolor sit amet")
   expect_output(str(SASxport::read.xport(tmp), "Lorem ipsum dolor sit amet"))
 
-  write_xport(df, path = tmp, label = "Lorem ipsum dolor sit amet")
+  xportr_write(df, path = tmp, label = "Lorem ipsum dolor sit amet")
   expect_error(
-    write_xport(df, tmp, label = "Lorizzle ipsizzle dolizzle pizzle go to hizzle"),
+    xportr_write(df, tmp, label = "Lorizzle ipsizzle dolizzle pizzle go to hizzle"),
     "must be 40 characters or less")
   expect_error(
-    write_xport(df, tmp, label = "Lorizzle ipsizzle dolizzl\xe7 pizzle"),
+    xportr_write(df, tmp, label = "Lorizzle ipsizzle dolizzl\xe7 pizzle"),
     "cannot contain any non-ASCII")
 
   df <- data.frame(loremipsum = "a", y_ = 1)
   SASxport::label(df$y_) <- "var2"
   expect_error(
-    write_xport(df, tmp),
+    xportr_write(df, tmp),
     "The following validation failed")
 })
