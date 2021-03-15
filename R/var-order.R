5f215094969e57db19b2c216a418006f9f5ea661
Ben Straub <ben.x.straub@gsk.com>
2021-03-14 22:00:20 -0400

    Cleaned up Switch Statement

--- R/var-order.R
+++ R/var-order.R
@@ -36,37 +36,7 @@ get_spec_col_names <- function(df1, tab_model, path_to_spec, vendor){
                     ds_sub$`Variable Name`}
   )
 }
-  # if (vendor == "CDISC"){
-  #   if (tab_model == "SDTM"){ 
-  #     spec_ds <- read_xlsx(paste0(path_to_spec, "/SDTM_spec.xlsx"), sheet = 3)
-  #     #spec_ds <- read_xlsx("~/xptr/inst/specs/SDTM_spec.xlsx", sheet = 3) 
-  #     ds_sub <- spec_ds[which(spec_ds$Dataset == df1), ]
-  #     ds_sub$Variable
-  #     
-  #   }
-  #   else if (tab_model == "ADAM"){ 
-  #     spec_ds <- read_xlsx(paste0(path_to_spec, "/ADaM_spec.xlsx"), sheet = 3)
-  #     #spec_ds <- read_xlsx("~/xptr/inst/specs/ADaM_spec.xlsx", sheet = 3) 
-  #     ds_sub <- spec_ds[which(spec_ds$Dataset == df1), ]
-  #     ds_sub$Variable
-  #   }
-  # }
-  # if (vendor == "GSK"){
-  #   if (tab_model == "SDTM"){
-  #     spec_ds <- read_xlsx(paste0(path_to_spec, "/gsk_all_specs.xlsx"), sheet = 1)
-  #     #spec_ds <- read_xlsx("~/xptr/inst/specs/gsk_all_specs.xlsx", sheet = 3) 
-  #     ds_sub <- spec_ds[which(spec_ds$Dataset == df1), ]
-  #     ds_sub$Variable
-  #     
-  #   }
-  #   else if (tab_model == "ADAM"){ 
-  #     spec_ds <- read_xlsx(paste0(path_to_spec, "/gsk_all_specs.xlsx"), sheet = 1)
-  #     #spec_ds <- read_xlsx("~/xptr/inst/specs/gsk_all_specs.xlsx") 
-  #     ds_sub <- spec_ds[which(spec_ds$`Domain Name` == df1), ]
-  #     ds_sub$`Variable Name`
-  #   }
-  # }
-#}
+
 
 #' @title Extract Variable Names from input dataset
 #' 
