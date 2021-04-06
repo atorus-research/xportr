c34221b04516bacb721b41da88585ae89bc0bb38
Eli Miller <Eli.Miller@atorusresearch.com>
2021-04-06 19:08:30 +0000

    Apply suggestions from code review

--- R/messages.R
+++ R/messages.R
@@ -2,8 +2,34 @@
 xportr_logger <- function(message, type = "none", ...) {
   
   log_fun <- switch(type,
-                    stop = rlang::abort,
-                    warn = rlang::warn,
+# inspired by
+# https://github.com/r-lib/gargle/blob/e2c7a48c208c3904d9038e8a7fd8d1045a4b3455/R/ui.R#L116
+cli_this = function(..., .envir = parent.frame()) {
+  txt <- cli::cli_format_method(cli::cli_text(..., .envir = .envir))
+  txt
+}
+
+xportr_stop = function(..., .envir = parent.frame()) {
+  txt <- purrr::map_chr(..., cli_this, .envir = .envir)
+  
+  if (length(txt) > 1L) {
+    paste(txt[[1]], rlang::format_error_bullets(txt[-1]), sep = "\n") %>% 
+      rlang::abort()
+  }
+  
+  rlang::abort(txt)
+}
+
+xportr_warn = function(..., .envir = parent.frame()) {
+  txt <- purrr::map_chr(..., cli_this, .envir = .envir)
+  
+  if (length(txt) > 1L) {
+    paste(txt[[1]], rlang::format_error_bullets(txt[-1]), sep = "\n") %>% 
+      rlang::warn()
+  }
+  
+  rlang::warn(txt)
+}
                     message = cli::cli_alert_info,
                     return())
   
