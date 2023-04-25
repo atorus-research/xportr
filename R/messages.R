e6e081ac2190d96c2b0495d7a11e1f329c32f13f
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-04-25 19:44:26 +0200

    revert removal of empty line

--- R/messages.R
+++ R/messages.R
@@ -10,12 +10,13 @@
 #' @return Output to Console
 #' @export
 xportr_logger <- function(message, type = "none", ...) {
+
   log_fun <- switch(type,
                     stop = abort,
                     warn = warn,
                     message = inform,
                     return())
-  
+
   do.call(log_fun, list(message, ...))
 
 }
@@ -28,23 +29,23 @@ xportr_logger <- function(message, type = "none", ...) {
 #' @return Output to Console
 #' @export
 var_names_log <- function(tidy_names_df, verbose) {
-  
-  
+
+
   only_renames <- tidy_names_df %>%
     filter(original_varname != renamed_var) %>%
     mutate(renamed_msg = paste0("Var ", col_pos, ":  '", original_varname,
                                 "' was renamed to '", renamed_var, "'"))
-  
+
   # Message regarding number of variables that were renamed/ modified
   num_renamed <- nrow(only_renames)
   tot_num_vars <- nrow(tidy_names_df)
   message("\n")
   cli::cli_h2(paste0(num_renamed, " of ", tot_num_vars, " (",
                      round(100 * (num_renamed / tot_num_vars), 1), "%) variables were renamed"))
-  
+
   # Message stating any renamed variables each original variable and it's new name
   if (nrow(only_renames) > 0) message(paste0(paste(only_renames$renamed_msg, collapse = "\n"), "\n"))
-  
+
   # Message checking for duplicate variable names after renamed (Pretty sure
   # this is impossible) but good to have a check none-the-less.
   dups <- tidy_names_df %>% filter(renamed_n > 1)
@@ -65,9 +66,9 @@ var_names_log <- function(tidy_names_df, verbose) {
 #' @return Output to Console
 #' @export
 type_log <- function(meta_ordered, type_mismatch_ind, verbose) {
-  
+
   if (length(type_mismatch_ind) > 0) {
-    
+
     message <- glue(
       "Variable type(s) in dataframe don't match metadata: ",
       paste0(glue("{encode_vars(meta_ordered[type_mismatch_ind, 'variable'])}"),
@@ -75,7 +76,7 @@ type_log <- function(meta_ordered, type_mismatch_ind, verbose) {
     )
 
     xportr_logger(message, verbose)
-    
+
     cli_h2("Variable type mismatches found.")
     cli_alert_success("{ length(type_mismatch_ind) } variables coerced")
   }
@@ -89,12 +90,12 @@ type_log <- function(meta_ordered, type_mismatch_ind, verbose) {
 #' @return Output to Console
 #' @export
 length_log <- function(miss_vars, verbose) {
-  
+
   if (length(miss_vars) > 0) {
-    
+
     cli_h2("Variable lengths missing from metadata.")
     cli_alert_success("{ length(miss_vars) } lengths resolved")
-    
+
     xportr_logger(
       c("Variable(s) present in dataframe but doesn't exist in `metadata`.",
         x = glue("Problem with {encode_vars(miss_vars)}")),
@@ -112,10 +113,10 @@ length_log <- function(miss_vars, verbose) {
 #' @export
 label_log <- function(miss_vars, verbose) {
   if (length(miss_vars) > 0) {
-    
+
     cli_h2("Variable labels missing from metadata.")
     cli_alert_success("{ length(miss_vars) } labels skipped")
-    
+
     xportr_logger(
       c("Variable(s) present in dataframe but doesn't exist in `metadata`.",
         x = glue("Problem with {encode_vars(miss_vars)}")),
@@ -133,7 +134,7 @@ label_log <- function(miss_vars, verbose) {
 #' @return Output to Console
 #' @export
 var_ord_msg <- function(moved_vars, verbose) {
-  
+
   if (moved_vars > 0) {
     cli_h2("{ length(moved_vars) } variables not in spec and moved to end")
     message <- glue(
