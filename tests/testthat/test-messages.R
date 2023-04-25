ef33801259424b4720bbd0341deb652020bd0d0d
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-04-26 00:34:15 +0200

    corrects linter problems

--- tests/testthat/test-messages.R
+++ tests/testthat/test-messages.R
@@ -1,5 +1,5 @@
 #' Test `R/messages.R` functions
-#' 
+#'
 #' Helper functions / data (from ´test/testthat/helper-mock_bindings.R´):
 #'  * \code{cli_mocked_fun} : function to replace `cli_\*()` calls functions
 #'    to avoid styling from theme and unnecessary empty lines
@@ -27,10 +27,10 @@ test_that("[length_log()] Missing lengths messages are shown", {
     stub(length_log, "cli_h2", cli_mocked_fun)
     stub(length_log, "cli_alert_success", cli_mocked_fun)
   }
-  
+
   length_log(c("var1", "var2", "var3"), "message") %>%
     expect_message("Variable lengths missing from metadata.") %>%
-    expect_message("lengths resolved") %>% 
+    expect_message("lengths resolved") %>%
     expect_message("Problem with `var1`.*`var2`.*`var3`")
 })
 
@@ -40,11 +40,11 @@ test_that("[length_log()] Missing variables messages are shown", {
     stub(label_log, "cli_h2", cli_mocked_fun)
     stub(label_log, "cli_alert_success", cli_mocked_fun)
   }
-  
+
   label_log(c("var1", "var2", "var3"), "message") %>%
     # cli messages
     expect_message("Variable labels missing from metadata.") %>%
-    expect_message("labels skipped") %>% 
+    expect_message("labels skipped") %>%
     # xportr_logger messages
     expect_message("Problem with `var1`.*`var2`.*`var3`")
 })
@@ -55,12 +55,12 @@ test_that("[var_ord_msg()] Reordered variables messages are shown", {
     stub(var_ord_msg, "cli_h2", cli_mocked_fun)
     stub(var_ord_msg, "cli_alert_success", cli_mocked_fun)
   }
-  
+
   moved_vars <- c("var1", "var2", "var3")
   message_regexp <- "Variable reordered in.+`var1`.+`var2`.+`var3`$"
 
   var_ord_msg(moved_vars, "message") %>%
-    expect_message("variables not in spec and moved to end") %>% 
+    expect_message("variables not in spec and moved to end") %>%
     expect_message(message_regexp)
 
   var_ord_msg(c(), "message") %>%
@@ -100,4 +100,3 @@ test_that("[var_names_log()] Renamed variables messages are shown", {
     expect_message("Var . : '.*' was renamed to '.*'") %>%
     expect_message("Duplicate renamed term\\(s\\) were created")
 })
-
