0e69d5f45dca230eedccce5a98985155ae506e5a
EeethB <ethanbrockmann@gmail.com>
2023-05-08 22:23:49 +0000

    Restore messages.R because somehow I broke it

--- R/messages.R
+++ R/messages.R
@@ -11,7 +11,8 @@
 #' @export
 xportr_logger <- function(message, type = "none", ...) {
 
-  log_fun <- switch(type,
+  log_fun <- switch(
+    type,
     stop = abort,
     warn = warn,
     message = inform,
@@ -19,6 +20,7 @@ xportr_logger <- function(message, type = "none", ...) {
   )
 
   do.call(log_fun, list(message, ...))
+
 }
 
 #' Utility for Renaming Variables
@@ -32,29 +34,40 @@ var_names_log <- function(tidy_names_df, verbose) {
 
   only_renames <- tidy_names_df %>%
     filter(original_varname != renamed_var) %>%
-    mutate(renamed_msg = paste0("Var ", col_pos, ":  '", original_varname,
-                                "' was renamed to '", renamed_var, "'"))
+    mutate(
+      renamed_msg = glue(
+        "Var {col_pos} : '{original_varname}' was renamed to 'renamed_var'"
+      )
+    )
 
   # Message regarding number of variables that were renamed/ modified
   num_renamed <- nrow(only_renames)
   tot_num_vars <- nrow(tidy_names_df)
-  message("\n")
-  cli::cli_h2(paste0(num_renamed, " of ", tot_num_vars, " (",
-                     round(100 * (num_renamed / tot_num_vars), 1), "%) variables were renamed"))
+
+  cli_h2(glue(
+    .sep = " ",
+    "{num_renamed} of {tot_num_vars}",
+    "({round(100*(num_renamed/tot_num_vars), 1)}%)",
+    "variables were renamed"
+  ))
 
   # Message stating any renamed variables each original variable and it's new name
   if (nrow(only_renames) > 0) {
-    purrr::walk(only_renames$renamed_msg, ~ xportr_logger(.x, verbose))
+    walk(only_renames$renamed_msg, ~xportr_logger(.x, verbose))
   }
 
   # Message checking for duplicate variable names after renamed (Pretty sure
   # this is impossible) but good to have a check none-the-less.
   dups <- tidy_names_df %>% filter(renamed_n > 1)
   if (nrow(dups) != 0) {
-    cli::cli_alert_danger(
-      paste("Duplicate renamed term(s) were created. Consider creating dictionary terms for:",
-                  paste(unique(dups$renamed_var), collapse = ", ")
-    ))
+    cli_alert_danger(
+      glue(
+        .sep = " ",
+        "Duplicate renamed term(s) were created.",
+        "Consider creating dictionary terms for:",
+        encode_vars(unique(dups$renamed_var))
+      )
+    )
   }
 }
 
@@ -67,11 +80,12 @@ var_names_log <- function(tidy_names_df, verbose) {
 #' @return Output to Console
 #' @export
 type_log <- function(meta_ordered, type_mismatch_ind, verbose) {
+
   if (length(type_mismatch_ind) > 0) {
+
     message <- glue(
       "Variable type(s) in dataframe don't match metadata: ",
-      paste0(glue("{encode_vars(meta_ordered[type_mismatch_ind, 'variable'])}"),
-             collapse = "", sep = " ")
+      "{encode_vars(meta_ordered[type_mismatch_ind, 'variable'])}"
     )
 
     xportr_logger(message, verbose)
@@ -89,13 +103,17 @@ type_log <- function(meta_ordered, type_mismatch_ind, verbose) {
 #' @return Output to Console
 #' @export
 length_log <- function(miss_vars, verbose) {
+
   if (length(miss_vars) > 0) {
+
     cli_h2("Variable lengths missing from metadata.")
     cli_alert_success("{ length(miss_vars) } lengths resolved")
 
     xportr_logger(
-      c("Variable(s) present in dataframe but doesn't exist in `metadata`.",
-        x = glue("Problem with {encode_vars(miss_vars)}")),
+      glue(
+        "Variable(s) present in dataframe but doesn't exist in `metadata`.",
+        "Problem with {encode_vars(miss_vars)}"
+      ),
       type = verbose
     )
   }
@@ -109,19 +127,19 @@ length_log <- function(miss_vars, verbose) {
 #' @return Output to Console
 #' @export
 label_log <- function(miss_vars, verbose) {
+
   if (length(miss_vars) > 0) {
+
     cli_h2("Variable labels missing from metadata.")
     cli_alert_success("{ length(miss_vars) } labels skipped")
 
     xportr_logger(
       c("Variable(s) present in dataframe but doesn't exist in `metadata`.",
-        x = glue("Problem with {encode_vars(miss_vars)}")
-      ),
+        x = glue("Problem with {encode_vars(miss_vars)}")),
       type = verbose
     )
   }
 }
-
 
 #' Utility for Ordering
 #'
@@ -131,16 +149,14 @@ label_log <- function(miss_vars, verbose) {
 #' @return Output to Console
 #' @export
 var_ord_msg <- function(moved_vars, verbose) {
+
   if (length(moved_vars) > 0) {
     cli_h2("{ length(moved_vars) } variables not in spec and moved to end")
     message <- glue(
-      "Variable reordered in `.df`: ",
-      paste0(glue("{ encode_vars(moved_vars) }"),
-             collapse = "", sep = " ")
+      "Variable reordered in `.df`: { encode_vars(moved_vars) }"
     )
     xportr_logger(message, verbose)
   } else {
     cli_h2("All variables in specification file are in dataset")
   }
-
 }
