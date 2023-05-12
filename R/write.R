551da6a820410c05504ba23812e9bc781bab56d9
Eli Miller <elimillera@gmail.com>
2023-05-12 09:08:07 -0500

    Update R/write.R
    Co-authored-by: Ben Straub <ben.x.straub@gsk.com>

--- R/write.R
+++ R/write.R
@@ -9,8 +9,8 @@
 #'   used as `xpt` name.
 #' @param label Dataset label. It must be<=40 characters.
 #' @param strict_checks If TRUE, xpt validation will report errors and not
-#'  the dataset. If FALSE, xpt validation will report warnings and continue
-#'  with writing. Defaults to FALSE
+#'  write out the dataset. If FALSE, xpt validation will report warnings and continue
+#'  with writing out the dataset. Defaults to FALSE
 #'
 #' @details
 #'   * Variable and dataset labels are stored in the "label" attribute.
