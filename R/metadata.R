d4cf362ede95c587ba30314245e84c8bb812137f
EeethB <ethanbrockmann@gmail.com>
2023-05-16 18:59:45 +0000

    Align metadata docs to style guide

--- R/metadata.R
+++ R/metadata.R
@@ -1,12 +1,16 @@
 #' Set variable specifications and domain
 #'
-#' @param .df An R object with columns that can be coerced
-#' @param metacore Either a data.frame that has the names of all possible columns
-#'   and their types, or a `Metacore` object from the `Metacore` package. Required
-#'   column names are dataset, variables, type
-#' @param domain Name of the dataset. Ex ADAE/DM. This will be used to subset
-#'   the metacore object. If none is passed it is assumed to be the name of the
-#'   dataset passed in `.df`.
+#' Sets metadata for a dataset in a way that can be accessed by other xportr
+#' functions. If used at the start of an xportr pipeline, it removes the need to
+#' set metadata and domain at each step individually
+#'
+#' @param .df A dataframe with columns that can be manipulated by other xportr
+#'   functions
+#' @param metadata An appropriate metadata object that has available
+#'   column-level metadata appropriate for use in other xportr functions
+#' @param domain Appropriate CDSIC dataset name, e.g. ADAE, DM. Used to subset
+#'   the metadata object. If none is passed, then name of the dataset passed .df
+#'   will be used.
 #'
 #' @return `.df` dataset with metadata and domain attributes set
 #' @export
@@ -15,7 +19,8 @@
 #' metadata <- data.frame(
 #'   dataset = "test",
 #'   variable = c("Subj", "Param", "Val", "NotUsed"),
-#'   type = c("numeric", "character", "numeric", "character")
+#'   type = c("numeric", "character", "numeric", "character"),
+#'   order = c(1, 3, 4, 2)
 #' )
 #'
 #' adlb <- data.frame(
@@ -26,7 +31,7 @@
 #' )
 #'
 #' xportr_metadata(adlb, metadata, "test")
-xportr_metadata <- function(.df, metacore, domain = NULL) {
+xportr_metadata <- function(.df, metadata, domain = NULL) {
   ## Common section to detect domain from argument or pipes
 
   df_arg <- tryCatch(as_name(enexpr(.df)), error = function(err) NULL)
@@ -35,5 +40,5 @@ xportr_metadata <- function(.df, metacore, domain = NULL) {
 
   ## End of common section
 
-  structure(.df, metadata = metacore)
+  structure(.df, metadata = metadata)
 }
