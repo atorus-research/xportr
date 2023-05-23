4384ec3c33474a42b5bc62078459891594f703a9
Sadchla Mathieu <sadchla.mathieu@gmail.com>
2023-05-23 08:17:52 -0400

    Non-ASCII symbols check

--- tests/testthat/test-utils-xportr.R
+++ tests/testthat/test-utils-xportr.R
@@ -95,9 +95,9 @@ test_that("xpt_validate: Get error message when the variable type is invalid", {
 
 test_that("xpt_validate: Get error message when the label contains non-ASCII, symbol or special characters", {
   df <- data.frame(A = 1, B = 2)
-  attr(df$A, "label") <- "foo%bar"
+  attr(df$A, "label") <- "foo&#191;bar"
   expect_equal(
     xpt_validate(df),
-    "Label 'A=foo%bar' cannot contain any non-ASCII, symbol or special characters."
+    "Label 'A=foo&#191;bar' cannot contain any non-ASCII, symbol or special characters."
   )
 })
