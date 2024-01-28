d64742e00943c8bbff361e8717029622733d3b7a
Sophie Shapcott <sophie.shapcott@gemprogramming.com>
2024-01-28 18:51:06 +0000

    #164 - add checks and messaging to `xportr_format`.

--- R/format.R
+++ R/format.R
@@ -90,11 +90,119 @@ xportr_format <- function(.df,
 
   names(format) <- filtered_metadata[[variable_name]]
 
+  # vector of expected formats for clinical trials (usually character or date/time)
+  # https://documentation.sas.com/doc/en/pgmsascdc/9.4_3.5/leforinforref/n0p2fmevfgj470n17h4k9f27qjag.htm#n0wi06aq4kydlxn1uqc0p6eygu75
+
+  expected_formats <- c(
+    NA,
+    "",
+    paste("$", 1:200, ".", sep = ""),
+    paste("date", 5:11, ".", sep = ""),
+    paste("time", 2:20, ".", sep = ""),
+    paste("datetime", 7:40, ".", sep = ""),
+    paste("yymmdd", 2:10, ".", sep = ""),
+    paste("mmddyy", 2:10, ".", sep = ""),
+    paste("ddmmyy", 2:10, ".", sep = ""),
+    "E8601DA.",
+    "E8601DA10.",
+    "E8601DN.",
+    "E8601DN10.",
+    "E8601TM.",
+    paste("E8601TM", 8:15, ".", sep = ""),
+    paste("E8601TM", 8:15, ".", 0:6, sep = ""),
+    "E8601TZ.",
+    paste("E8601TZ", 9:20, ".", sep = ""),
+    paste("E8601TZ", 9:20, ".", 0:6, sep = ""),
+    "E8601TX.",
+    paste("E8601TX", 9:20, ".", sep = ""),
+    "E8601DT.",
+    paste("E8601DT", 16:26, ".", sep = ""),
+    paste("E8601DT", 16:26, ".", 0:6, sep = ""),
+    "E8601LX.",
+    paste("E8601LX", 20:35, ".", sep = ""),
+    "E8601LZ.",
+    paste("E8601LZ", 9:20, ".", sep = ""),
+    "E8601DX.",
+    paste("E8601DX", 20:35, ".", sep = ""),
+    "B8601DT.",
+    paste("B8601DT", 15:26, ".", sep = ""),
+    paste("B8601DT", 15:26, ".", 0:6, sep = ""),
+    "IS8601DA.",
+    "B8601DA.",
+    paste("B8601DA", 8:10, ".", sep = ""),
+    "weekdate.",
+    paste("weekdate", 3:37, ".", sep = ""),
+    "mmddyy.",
+    "ddmmyy.",
+    "yymmdd.",
+    "date.",
+    "time.",
+    "hhmm.",
+    "IS8601TM.",
+    "E8601TM.",
+    "B8601TM."
+  )
+
+  # w.d format for numeric variables
+  format_regex <- "^([1-9]|[12][0-9]|3[0-2])\\.$|^([1-9]|[12][0-9]|3[0-2])\\.([1-9]|[12][0-9]|3[0-1])$"
+
+
   for (i in seq_len(ncol(.df))) {
     format_sas <- purrr::pluck(format, colnames(.df)[i])
     if (is.na(format_sas) || is.null(format_sas)) {
       format_sas <- ""
     }
+    # series of checks for formats
+
+    # check that any variables ending DT, DTM, TM have a format
+    if (grepl("DT$|DTM$|TM$", colnames(.df)[i]) == TRUE & format_sas == ""){
+      cli_alert_danger(
+        "(xportr::xportr_format) {encode_vars(colnames(.df)[i])} is expected to have a format but does not."
+      )
+    }
+
+    # remaining checks to be carried out if a format exists
+    if (format_sas != "") {
+      # if the variable is character
+      if (class(.df[[i]])[1] == "character") {
+        # character variable formats should start with a $
+        if (grepl("^\\$", format_sas) == FALSE) {
+          cli_alert_danger(
+            "(xportr::xportr_format) {encode_vars(colnames(.df)[i])} is a character variable and should have a `$` prefix."
+          )
+        }
+        # character variable formats should have length <= 31 (excluding the $)
+        if (nchar(format_sas) > 32) {
+          cli_alert_danger(
+            "(xportr::xportr_format) Format for character variable {encode_vars(colnames(.df)[i])} should have length <= 31 (excluding `$`)."
+          )
+        }
+      }
+
+      # if the variable is numeric
+      if (class(.df[[i]])[1] == "numeric") {
+      # numeric variables should not start with a $
+        if (grepl("^\\$", format_sas) == TRUE) {
+          cli_alert_danger(
+            "(xportr::xportr_format) {encode_vars(colnames(.df)[i])} is a numeric variable and should not have a `$` prefix."
+          )
+        }
+        # numeric variable formats should have length <= 32
+        if (nchar(format_sas) > 32) {
+          cli_alert_danger(
+            "(xportr::xportr_format) Format for numeric variable {encode_vars(colnames(.df)[i])} should have length <= 32."
+          )
+        }
+      }
+
+      # check if the format is either one of the expected formats or follows the regular expression for w.d format
+      if (!(format_sas %in% toupper(expected_formats)) & (stringr::str_detect(format_sas, pattern = format_regex) == FALSE)) {
+        cli_alert_info(
+          "(xportr::xportr_format) Check format {encode_vars(format_sas)} for variable {encode_vars(colnames(.df)[i])} - is this correct?"
+        )
+      }
+    }
+
     attr(.df[[i]], "format.sas") <- format_sas
   }
 
