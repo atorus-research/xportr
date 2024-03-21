9aa991fbeca67f8a1994d93e35e21d983b2b588c
Eli Miller <elimillera@gmail.com>
2024-03-20 21:16:18 -0500

    Doc updates per #261

--- R/xportr-package.R
+++ R/xportr-package.R
@@ -127,7 +127,7 @@ globalVariables(c(
   "abbr_parsed", "abbr_stem", "adj_orig", "adj_parsed", "col_pos", "dict_varname",
   "lower_original_varname", "my_minlength", "num_st_ind", "original_varname",
   "renamed_n", "renamed_var", "use_bundle", "viable_start", "type.x", "type.y",
-  "variable", "length.x", "lenght.y", "e", "length_df", "length_meta"
+  "variable", "length.x", "lenght.y", "e", "length_df", "length_meta", ".env"
 ))
 
 # The following block is used by usethis to automatically manage
