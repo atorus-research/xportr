a18cda9c49764bae64aa24c6bf9bb9b88a950a22
Adrian Chan <adrian.chan@roche.com>
2024-09-13 18:23:11 +0000

    updates

--- R/split.R
+++ R/split.R
@@ -3,6 +3,9 @@
 #' @description
 #' `r lifecycle::badge("deprecated")`
 #'
+#' This function is *deprecated*. Please use the argument
+#'  `max_gb_size` in the function xportr_write()` instead.
+#'
 #' Per the FDA Study Data Technical Conformance
 #' Guide(https://www.fda.gov/media/88173/download) section 3.3.2, dataset files
 #' sizes shouldn't exceed 5 GB. If datasets are large enough, they should be
@@ -32,7 +35,14 @@
 #'
 #' adlb <- xportr_split(adlb, "LBCAT")
 xportr_split <- function(.df, split_by = NULL) {
-  .Deprecated("xportr_write(max_size_gb = ..")
+  lifecycle::deprecate_warn(
+    when = "0.5.0",
+    what = "xportr_split()",
+    with = "xportr_write()",
+    details = "Please use the argument `max_gb_size` in the
+    function xportr_write() instead` instead."
+  )
+
   attr(.df, "_xportr.split_by_") <- split_by
   return(.df)
 }
