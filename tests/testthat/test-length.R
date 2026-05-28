1acf76c1be13f3bddb634dcc597ca53bc4a7b11c
Nimisha Majumder <nimishamajumder@gmail.com>
2026-05-29 03:56:27 +0530

    fixed lintr and code style checks for new length tests

--- tests/testthat/test-length.R
+++ tests/testthat/test-length.R
@@ -283,8 +283,10 @@ test_that("length Test 12: Throws message when metadata variables not present in
 
 ## Test 13: xportr_length: Works as expected with single row dataset ----
 test_that("length Test 13: Works as expected with single row dataset", {
-  adsl <- data.frame(n1 = c(123),
-                     c1 = c("abc"))
+  adsl <- data.frame(
+    n1 = c(123),
+    c1 = c("abc")
+  )
 
   metadata <- tribble(
     ~dataset, ~variable, ~length, ~type,
@@ -292,8 +294,8 @@ test_that("length Test 13: Works as expected with single row dataset", {
     "adsl", "c1", 200, "character",
   )
 
-  xportr_length(adsl, metadata, domain = "adsl") %>%
-    expect_silent() %>%
+  xportr_length(adsl, metadata, domain = "adsl") |>
+    expect_silent() |>
     expect_attr_width(metadata$length)
 })
 
@@ -308,35 +310,41 @@ test_that("length Test 14: Works as expected with mixed NA and non-NA lengths in
   )
 
   # Case 1: Variables with only NA values
-  adsl_na <- data.frame(n1 = c(NA, NA),
-                        c1 = c(NA_character_, NA_character_),
-                        n2 = c(NA, NA),
-                        c2 = c(NA_character_, NA_character_))
+  adsl_na <- data.frame(
+    n1 = c(NA, NA),
+    c1 = c(NA_character_, NA_character_),
+    n2 = c(NA, NA),
+    c2 = c(NA_character_, NA_character_)
+  )
 
   # Case 2: Variables with non-NA values
-  adsl_val <- data.frame(n1 = c(123, 456),
-                         c1 = c("abc", "def"),
-                         n2 = c(789, 910),
-                         c2 = c("ghi", "jkl"))
+  adsl_val <- data.frame(
+    n1 = c(123, 456),
+    c1 = c("abc", "def"),
+    n2 = c(789, 910),
+    c2 = c("ghi", "jkl")
+  )
 
 
   suppressMessages({
-    xportr_length(adsl_na, metadata, domain = "adsl", length_source = "metadata") %>%
+    xportr_length(adsl_na, metadata, domain = "adsl", length_source = "metadata") |>
       expect_attr_width(c(8, 200, 8, 0))
-    xportr_length(adsl_na, metadata, domain = "adsl", length_source = "data") %>%
+    xportr_length(adsl_na, metadata, domain = "adsl", length_source = "data") |>
       expect_attr_width(c(8, 0, 8, 0))
 
-    xportr_length(adsl_val, metadata, domain = "adsl", length_source = "metadata") %>%
+    xportr_length(adsl_val, metadata, domain = "adsl", length_source = "metadata") |>
       expect_attr_width(c(8, 200, 8, 3))
-    xportr_length(adsl_val, metadata, domain = "adsl", length_source = "data") %>%
+    xportr_length(adsl_val, metadata, domain = "adsl", length_source = "data") |>
       expect_attr_width(c(8, 3, 8, 3))
   })
 })
 
 ## Test 15: xportr_length: Works as expected with zero-length character columns in dataset ----
 test_that("length Test 15: Works as expected with zero-length character columns in dataset", {
-  adsl <- data.frame(b = c("", ""),
-                     undefined_char_var = c("", ""))
+  adsl <- data.frame(
+    b = c("", ""),
+    undefined_char_var = c("", "")
+  )
 
   metadata <- minimal_metadata(
     dataset = TRUE,
@@ -345,9 +353,9 @@ test_that("length Test 15: Works as expected with zero-length character columns 
   )
 
   suppressMessages({
-    xportr_length(adsl, metadata, domain = "adsl") %>%
+    xportr_length(adsl, metadata, domain = "adsl") |>
       expect_attr_width(c(200, 0))
-    xportr_length(adsl, metadata, domain = "adsl", length_source = "data") %>%
+    xportr_length(adsl, metadata, domain = "adsl", length_source = "data") |>
       expect_attr_width(c(0, 0))
   })
 })
@@ -364,15 +372,17 @@ test_that("length Test 16: Length in metadata or dataset exceeds SAS limits", {
     "adsl", "N1", 10000, "numeric"
   )
 
-  xportr_length(adsl, metadata, domain = "adsl") %>%
-    expect_attr_width(metadata$length) %>%
+  xportr_length(adsl, metadata, domain = "adsl") |>
+    expect_attr_width(metadata$length) |>
     expect_silent()
 
   # Case 2: Variable length exceeds SAS limits in dataset (with defined or undefined attributes in metadata)
-  adsl <- data.frame(C1 = wide_str,
-                     C2 = wide_str,
-                     N1 = 99999999999,
-                     N2 = 99999999999)
+  adsl <- data.frame(
+    C1 = wide_str,
+    C2 = wide_str,
+    N1 = 99999999999,
+    N2 = 99999999999
+  )
   metadata <- tribble(
     ~dataset, ~variable, ~length, ~type,
     "adsl", "C1", 200, "character",
@@ -380,7 +390,7 @@ test_that("length Test 16: Length in metadata or dataset exceeds SAS limits", {
   )
 
   suppressMessages(
-    xportr_length(adsl, metadata, domain = "adsl") %>%
+    xportr_length(adsl, metadata, domain = "adsl") |>
       expect_attr_width(c(200, 1000, 8, 8))
   )
 })
@@ -392,11 +402,10 @@ test_that("length Test 17: Gets warning/error as expected for verbose modes 'war
   metadata <- minimal_metadata(dataset = TRUE, length = TRUE, var_names = "x")
 
   suppressMessages({
-  xportr_length(adsl, metadata, domain = "adsl", verbose = "warn") %>%
-    expect_warning()
+    xportr_length(adsl, metadata, domain = "adsl", verbose = "warn") |>
+      expect_warning()
 
-  xportr_length(adsl, metadata, domain = "adsl", verbose = "stop") %>%
-    expect_error()
+    xportr_length(adsl, metadata, domain = "adsl", verbose = "stop") |>
+      expect_error()
   })
-
 })
